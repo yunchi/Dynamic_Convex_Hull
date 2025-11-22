05b9da56ca557392419014f3f3b6d80744322657
Yun Chi <yunchi@Yuns-Mac-mini.local>
2025-11-21 19:37:34 -0800

    Extend the coordinate comparison to include y. Note: Need follow-ups with the subhull logic, as described by the following warning.
    WARNING
    
    Risk to ConvexHull Stability Although the existing tests passed, please remember that
    ConvexHull
     assumes strictly increasing x-coordinates. Inserting points with the same x-coordinate (which is now possible) may cause SubHull.computeSlope to fail or produce incorrect results. Please ensure that the application logic prevents inserting such points if the convex hull algorithm does not support them.

--- src/main/java/com/github/yunchi/dynamic_convex_hull/Coordinate2D.java
+++ src/main/java/com/github/yunchi/dynamic_convex_hull/Coordinate2D.java
@@ -4,39 +4,43 @@ public class Coordinate2D implements Comparable<Coordinate2D> {
 
 	/**
 	 * @author Yun Chi
-	 * @since 2012-10-12 
+	 * @since 2012-10-12
 	 */
-	
+
 	protected final double x;
 	protected final double y;
-	
+
 	public Coordinate2D(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public Coordinate2D(Coordinate2D other) {
 		this.x = other.x;
 		this.y = other.y;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
-	public int compareTo(Coordinate2D other) {//shallow comparison
+
+	public int compareTo(Coordinate2D other) {// shallow comparison
 		if (this.x < other.x) {
 			return -1;
-		}
-		else if (this.x > other.x) {
+		} else if (this.x > other.x) {
 			return +1;
-		}
-		else {
-			return 0;
+		} else {
+			if (this.y < other.y) {
+				return -1;
+			} else if (this.y > other.y) {
+				return +1;
+			} else {
+				return 0;
+			}
 		}
 	}
 
--- src/test/java/com/github/yunchi/dynamic_convex_hull/Coordinate2DTest.java
+++ src/test/java/com/github/yunchi/dynamic_convex_hull/Coordinate2DTest.java
@@ -11,22 +11,22 @@ public class Coordinate2DTest {
 		Coordinate2D c1 = new Coordinate2D(1.5, 2.5);
 		assertEquals(1.5, c1.getX(), 0.000001);
 		assertEquals(2.5, c1.getY(), 0.000001);
-		
+
 		Coordinate2D c2 = new Coordinate2D(c1);
 		assertEquals(1.5, c2.getX(), 0.000001);
 		assertEquals(2.5, c2.getY(), 0.000001);
 	}
-	
+
 	@Test
 	public void testCompareTo() {
 		Coordinate2D c1 = new Coordinate2D(1.0, 2.0);
 		Coordinate2D c2 = new Coordinate2D(2.0, 2.0);
 		Coordinate2D c3 = new Coordinate2D(1.0, 3.0);
 		Coordinate2D c4 = new Coordinate2D(0.0, 2.0);
-		
+
 		assertTrue(c1.compareTo(c2) < 0);
 		assertTrue(c2.compareTo(c1) > 0);
-		assertTrue(c1.compareTo(c3) == 0); // Only compares x
+		assertTrue(c1.compareTo(c3) < 0); // Now compares y as well
 		assertTrue(c1.compareTo(c4) > 0);
 		assertTrue(c1.compareTo(c1) == 0);
 	}
